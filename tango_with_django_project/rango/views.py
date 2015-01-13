4c9bbf8321bde6c83689a3afba756950524e72bd
Isaac Jordan <sheepses6@gmail.com>
2015-01-13 15:44:19 +0000

    Chapter 4 - completed

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there world!")
+    return HttpResponse("Rango says hey there world! <br><a href='/rango/about'>About</a>")
+
+def about(request):
+    return HttpResponse("Rango says here is the about page. <br>This tutorial has been put together by Isaac Jordan, 2080466. <br><a href='/rango'>Main Page</a>")
