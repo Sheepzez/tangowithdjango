68758444bce7410adccd081f5bfbc354f3dc1312
Isaac Jordan <sheepses6@gmail.com>
2015-01-27 23:58:44 +0000

    Created requirements.txt, completed up to Chapter 9.4, ignore swap files.

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -10,4 +10,5 @@ urlpatterns = patterns('',
             ),
         url(r'^add_category/$', views.add_category, name='add_category'),
         url(r'^category/(?P<category_name_slug>.*)/add_page', views.add_page, name='add_page'),
+	url(r'^register/$', views.register, name='register'),
 )
