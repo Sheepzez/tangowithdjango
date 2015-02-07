bbb8f62528c00e2983bb3ebd84adf105c6f004b9
Isaac Jordan <sheepses6@gmail.com>
2015-02-07 20:41:56 +0000

    Chapter 10 - completed

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -19,7 +19,7 @@ def about(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html')
 
 def category(request, category_name_slug):
     args = {}
