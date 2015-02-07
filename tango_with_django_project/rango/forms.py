b809df6fb70115ca67cfc0ce4b8970fd2591701f
Isaac Jordan <sheepses6@gmail.com>
2015-02-07 19:44:23 +0000

    Chapter 9 - completed.

--- tango_with_django_project/rango/forms.py
+++ tango_with_django_project/rango/forms.py
@@ -17,8 +17,18 @@ class CategoryForm(forms.ModelForm):
 
 class PageForm(forms.ModelForm):
     title = forms.CharField(max_length=128, help_text="Please enter the title of the page.")
-    url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
+    url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.", initial='http://')
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+    #This doesn't work since HTML5 url input fields do URL validation.
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        # If url is not empty and doesn't start with 'http://', prepend 'http://'.
+        if url and not url.startswith('http://'):
+            url = 'http://' + url
+            cleaned_data['url'] = url
 
     class Meta:
         # Provide an association between the ModelForm and a model
@@ -32,17 +42,6 @@ class PageForm(forms.ModelForm):
         exclude = ('category',)
         #or specify the fields to include (i.e. not include the category field)
         #fields = ('title', 'url', 'views')
-
-    def clean(self):
-        cleaned_data = self.cleaned_data
-        url = cleaned_data.get('url')
-
-        # If url is not empty and doesn't start with 'http://', prepend 'http://'.
-        if url and not url.startswith('http://'):
-            url = 'http://' + url
-            cleaned_data['url'] = url
-
-        return cleaned_data
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
