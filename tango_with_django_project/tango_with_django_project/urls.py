8984d08ab6cc7c9f67ca12c9e9c8c11169e4aaf0
Isaac Jordan <sheepses6@gmail.com>
2015-02-23 17:59:55 +0000

    Chapter 12 - completed (with exercise).

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.conf import settings
 
 from registration.backends.simple.views import RegistrationView
+from rango import views as rangoViews
 
 # Create a new class that redirects the user to the index page, if successful at logging
 class MyRegistrationView(RegistrationView):
@@ -13,6 +14,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^rango/', include('rango.urls')),
     url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
+    url(r'^accounts/password/change/done/$', rangoViews.passwordChangeDone, name='auth_password_change_done'),
     (r'^accounts/', include('registration.backends.simple.urls')),
 )
 
